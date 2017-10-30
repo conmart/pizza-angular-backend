d6a896db583f96f5198f1f0683836cced05ae964
mnfmnfm <mnfmnfm@gmail.com>
2017-10-30 15:40:28 -0700

    add auth

--- controllers/pizza.js
+++ controllers/pizza.js
@@ -1,23 +1,29 @@
 var db = require('../models');
 
 function index(req, res) {
-  console.log("session is", req.sessionStore, req.session)
+
+  console.log("session is", req.session)
   console.log("user is", req.session.passport && req.session.passport.user)
-  db.Pizza.find({}, function(err, foundPizzas) {
-    if (err) {
-      console.log("error");
-      res.setStatus(400).send(err);
-    } else {
-      res.send(foundPizzas);
-    }
-  })
+  console.log("authentication status is", req.isAuthenticated())
+  if (!req.isAuthenticated()) {
+    res.status(403).send("Please log in before fetching pizzas.")
+  } else {
+    db.Pizza.find({}, function(err, foundPizzas) {
+      if (err) {
+        console.log("error");
+        res.status(400).send(err);
+      } else {
+        res.send(foundPizzas);
+      }
+    })
+  }
 }
 
 function create(req, res) {
   db.Pizza.create(req.body, function(err, savedPizza){
     if (err) {
       console.log("error");
-      res.setStatus(400).send(err);
+      res.status(400).send(err);
     } else {
       res.send(savedPizza);
     }
